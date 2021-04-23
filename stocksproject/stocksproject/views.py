a0d7f285cc2ebf89f2c2554916c259cf168c9019
Francis Severino <francisjsg2@gmail.com>
2021-04-23 11:44:51 -0400

    Rendering page on index

--- stocksproject/stocksproject/views.py
+++ stocksproject/stocksproject/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+def stocks_view(request):
+    # create a dictionary to pass
+    # data to the template
+    # return response with template and context
+    return render(request, "index.html")
