54683833038f4ae9b4ce535787141619229e90ef
Francis Severino <francisjsg2@gmail.com>
2021-05-03 23:29:21 -0400

    added path for static files, and allow heroku url

--- stocksproject/settings.py
+++ stocksproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%b725sd0rj5(^&a)%b=53!b8!qh#(az_zc-04-869-uvc&d)l1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://murmuring-headland-00227.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
