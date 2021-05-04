90fcb50ca714766b503dc2bc31f4f92adbb44411
Francis Severino <francisjsg2@gmail.com>
2021-05-03 20:13:31 -0400

    Added example of how to use api

--- stocksproject/stocksproject/views.py
+++ stocksproject/stocksproject/views.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
 from django.shortcuts import render
+import finnhub
 
 def stocks_view(request):
     # create a dictionary to pass
     # data to the template
     # return response with template and context
-    return render(request, "index.html")
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+
+    finnhub_client = finnhub.Client(api_key=api_key)
+    aapl = finnhub_client.quote('AAPL')
+    context = {'aapl': aapl}
+    return render(request, "index.html", context)
