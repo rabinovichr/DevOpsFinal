2da9604c2a598e27f62c0061fc334a131fbb7ed3
Francis Severino <francisjsg2@gmail.com>
2021-05-04 00:36:10 -0400

    added stocks to html

--- stocksproject/stocksproject/views.py
+++ stocksproject/stocksproject/views.py
@@ -4,48 +4,31 @@ from django.shortcuts import render
 import finnhub
 
 def stocks_view(request):
-    # create a dictionary to pass
-    # data to the template
-    # return response with template and context
     load_dotenv()
-    api_key = os.getenv('c1s391aad3i8lrmfh4o0')
+    api_key = os.getenv('API_KEY')
 
     finnhub_client = finnhub.Client(api_key=api_key)
 
-    aapl = finnhub_client.quote('AAPL')
+    aapl = finnhub_client.quote('AAPL')['c']
+    tsla = finnhub_client.quote('TSLA')['c']
+    amzn = finnhub_client.quote('AMZN')['c']
+    gme = finnhub_client.quote('GME')['c']
+    msft = finnhub_client.quote('MSFT')['c']
+    sbux = finnhub_client.quote('SBUX')['c']
+    amc = finnhub_client.quote('AMC')['c']
+    nflx = finnhub_client.quote('NFLX')['c']
+    ba = finnhub_client.quote('BA')['c']
+    prty = finnhub_client.quote('PRTY')['c']
 
-
-
-    tsla = finnhub_client.quote('TSLA')
-
-
-
-    amzn = finnhub_client.quote('AMZN')
-
-
-
-    gme = finnhub_client.quote('GME')
-
-
-
-    msft = finnhub_client.quote('MSFT')
-
-
-    sbux = finnhub_client.quote('SBUX')
-
-
-
-    amc = finnhub_client.quote('AMC')
-
-
-    nflx = finnhub_client.quote('NFLX')
-
-
-
-    ba = finnhub_client.quote('BA')
-    
-
-
-    prty = finnhub_client.quote('PRTY')
-
-    return render(request, "index.html", {'aapl': aapl,'tsla': tsla, 'amzn': amzn, 'gme': gme, 'msft': msft,'sbux': sbux, 'amc': amc, 'nflx': nflx, 'ba': ba, 'prty': prty})
+    return render(request, "index.html", {
+        'aapl': aapl,
+        'tsla': tsla, 
+        'amzn': amzn, 
+        'gme': gme, 
+        'msft': msft,
+        'sbux': sbux, 
+        'amc': amc, 
+        'nflx': nflx, 
+        'ba': ba, 
+        'prty': prty
+        })
